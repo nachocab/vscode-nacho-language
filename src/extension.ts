448917a105dd2993a4da833aac175125052d586a
Nacho Caballero <nachocab@gmail.com>
2023-11-08 18:48:07 +0100

    modernize documentsymbol

--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,42 @@
-'use strict';
-import * as vscode from 'vscode';
+// Code based on https://stackoverflow.com/a/46054953/355567
+"use strict";
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.languages.registerDocumentSymbolProvider(
-      { language: 'nacho' },
-      new DocumentSymbolProvider()
+      { language: "nacho" },
+      new NachoDocumentSymbolProvider()
     )
   );
 }
 
-class DocumentSymbolProvider implements vscode.DocumentSymbolProvider {
+class NachoDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
   public provideDocumentSymbols(
     document: vscode.TextDocument,
     token: vscode.CancellationToken
-  ): Thenable<vscode.SymbolInformation[]> {
+  ): Promise<vscode.DocumentSymbol[]> {
     return new Promise((resolve, reject) => {
-      let symbols = [];
-    
+      let symbols: vscode.DocumentSymbol[] = [];
+
       for (let i = 0; i < document.lineCount; i++) {
         const line = document.lineAt(i);
-        if (line.text.match('^ *#{2,}')) {
+        if (line.text.match("^ *#{2,}")) {
           const name = line.text
-            .replace(/#+ /, '')
-            .replace(/^( *)(.+)/g, function(match, first, rest) {
-              return `${'.'.repeat(first.length)}${rest}`;
+            .replace(/#+ /, "")
+            .replace(/^( *)(.+)/g, function (match, first, rest) {
+              return `${" ".repeat(first.length)}${rest}`;
             });
-          symbols.push({
-            name: name,
-            kind: vscode.SymbolKind.Field,
-            location: new vscode.Location(document.uri, line.range)
-          });
+
+          symbols.push(
+            new vscode.DocumentSymbol(
+              name,
+              "Heading",
+              vscode.SymbolKind.Field,
+              line.range,
+              line.range
+            )
+          );
         }
       }
 
