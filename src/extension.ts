3e05bb08ea56e6a18d87e8b0555c7ba9d58d59d2
Nacho Caballero <nachocab@gmail.com>
2023-11-09 13:37:13 +0100

    initial attempt at building tree

--- src/extension.ts
+++ src/extension.ts
@@ -11,39 +11,59 @@ export function activate(context: vscode.ExtensionContext) {
   );
 }
 
+interface Node {
+  name: string;
+  level: number;
+  startLine: number;
+  endCharacter: number;
+  parent: number;
+}
+
 class NachoDocumentSymbolProvider implements vscode.DocumentSymbolProvider {
   public provideDocumentSymbols(
     document: vscode.TextDocument,
     token: vscode.CancellationToken
   ): Promise<vscode.DocumentSymbol[]> {
+    // const nodes: Node[] = [];
+    // for (let i = 0; i < document.lineCount; i++) {
+    //   const line = document.lineAt(i);
+    //   let match;
+    //   if ((match = line.text.match("^\\s*(#{2,})\\s*(.+)?"))) {
+    //     const level = match[1]?.length;
+    //     const name = match[2] || `H${level}`;
+
+    //     nodes.push({
+    //       name,
+    //       level,
+    //       firstLine: line.range.start.line,
+    //       lastCharacter: line.range.end.character,
+    //       parent: nodes.findLastIndex((n: Node) => n.level > level),
+    //     });
+    //   }
+    // }
+
     const symbols: vscode.DocumentSymbol[] = [];
-    for (let i = 0; i < document.lineCount; i++) {
-      const line = document.lineAt(i);
-      let match;
-      if ((match = line.text.match("^\\s*(#{2,})"))) {
-        const level = match[1]?.length;
-        const name = line.text.replace(/^\s*#+ */, "") || `H${level}`;
+    // const symbols: vscode.DocumentSymbol[] = getSymbols(nodes);
 
-        const symbol = new vscode.DocumentSymbol(
-          name,
-          "",
-          vscode.SymbolKind.Field,
-          line.range,
-          line.range
-        );
-        // symbol.children.push(
-        //   new vscode.DocumentSymbol(
-        //     name,
-        //     "test",
-        //     vscode.SymbolKind.Field,
-        //     line.range,
-        //     line.range
-        //   )
-        // );
-        symbols.push(symbol);
-      }
-    }
-
+    // // return new Promise((resolve, reject) => resolve(symbols));
     return new Promise((resolve, reject) => resolve(symbols));
   }
+}
+
+function getSymbols(nodes: Node[]) {
+  return nodes.map((node) => {
+    const range = new vscode.Range(
+      node.startLine,
+      0,
+      nodes[node.lastChild]?.startLine,
+      node.endCharacter
+    );
+    return new vscode.DocumentSymbol(
+      node.name,
+      nodes[node.parent].name,
+      vscode.SymbolKind.Field,
+      range,
+      range
+    );
+  });
 }
